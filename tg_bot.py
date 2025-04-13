5a5c530b4971e4af213f598d417b6d4a8077095a
Yuriy Mikhailov <apofiz2036@bk.ru>
2025-04-13 10:55:30 +0500

    setup DialogFlow

--- tg_bot.py
+++ tg_bot.py
@@ -1,9 +1,9 @@
 import logging
-
 import os
 from dotenv import load_dotenv
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from google.cloud import dialogflow
 
 
 logging.basicConfig(
@@ -25,25 +25,44 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+def get_dialogflow_response(text, session_id, project_id):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code="ru")
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    return response.query_result.fulfillment_text    
+
+
+def handle_message(update: Update, context: CallbackContext) -> None:
+    user_text = update.message.text
+    user_id = str(update.message.from_user.id)
+
+    project_id = context.bot_data.get('project_id')
+    bot_response = get_dialogflow_response(user_text, user_id, project_id)
+    update.message.reply_text(bot_response)
 
 
 def main() -> None:
     load_dotenv()
     TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_TOKEN']
+    PROJECT_ID = os.environ['DIALOGFLOW_PROJECT_ID']
 
     updater = Updater(TELEGRAM_BOT_TOKEN)
-
     dispatcher = updater.dispatcher
+
+    dispatcher.bot_data['project_id'] = PROJECT_ID
 
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
-
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
 
     updater.start_polling()
-
     updater.idle()
 
 
