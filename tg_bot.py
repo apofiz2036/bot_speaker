ae5f134c9de664888a5c7c31135a9b886b923f5e
Yuriy Mikhailov <apofiz2036@bk.ru>
2025-04-15 08:13:11 +0500

    add logger

--- tg_bot.py
+++ tg_bot.py
@@ -1,16 +1,22 @@
 import logging
 import os
+from logging.handlers import RotatingFileHandler
+
 from dotenv import load_dotenv
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from google.cloud import dialogflow
 
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
 
-logger = logging.getLogger(__name__)
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -26,44 +32,69 @@ def help_command(update: Update, context: CallbackContext) -> None:
 
 
 def get_dialogflow_response(text, session_id, project_id):
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
+    try:
+        session_client = dialogflow.SessionsClient()
+        session = session_client.session_path(project_id, session_id)
 
-    text_input = dialogflow.TextInput(text=text, language_code="ru")
-    query_input = dialogflow.QueryInput(text=text_input)
+        text_input = dialogflow.TextInput(text=text, language_code="ru")
+        query_input = dialogflow.QueryInput(text=text_input)
 
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
+        )
 
-    return response.query_result.fulfillment_text
+        return response.query_result.fulfillment_text
+    except Exception as e:
+        logger = logging.getLogger(__name__)
+        logger.error(f"Ошибка в Dialogflow: {e}")
+        return 'Произошла ошибка'
 
 
 def handle_message(update: Update, context: CallbackContext) -> None:
-    user_text = update.message.text
-    user_id = str(update.message.from_user.id)
+    try:
+        user_text = update.message.text
+        user_id = str(update.message.from_user.id)
 
-    project_id = context.bot_data.get('project_id')
-    bot_response = get_dialogflow_response(user_text, user_id, project_id)
-    update.message.reply_text(bot_response)
+        project_id = context.bot_data.get('project_id')
+        bot_response = get_dialogflow_response(user_text, user_id, project_id)
+        update.message.reply_text(bot_response)
+    except Exception as e:
+        logger = logging.getLogger(__name__)
+        logger.error(f'Ошибка при обработке сообщения: {e}')
+        update.message.reply_text('Произошла ошибка')
 
 
 def main() -> None:
     load_dotenv()
     TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_TOKEN']
     PROJECT_ID = os.environ['DIALOGFLOW_PROJECT_ID']
+    TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.WARNING)
+
+    file_handler = RotatingFileHandler('bot.log', maxBytes=20000, backupCount=2)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(file_handler)
 
     updater = Updater(TELEGRAM_BOT_TOKEN)
     dispatcher = updater.dispatcher
 
+    telegram_handler = TelegramLogsHandler(updater.bot, TELEGRAM_CHAT_ID)
+    telegram_handler.setLevel(logging.ERROR)
+    logger.addHandler(telegram_handler)
     dispatcher.bot_data['project_id'] = PROJECT_ID
 
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_message))
 
-    updater.start_polling()
-    updater.idle()
+    try:
+        logger.info('Бот запущен')
+        updater.start_polling()
+        updater.idle()
+    except Exception as e:
+        logger.error(f"Бот упал с ошибкой: {e}")
 
 
 if __name__ == '__main__':
